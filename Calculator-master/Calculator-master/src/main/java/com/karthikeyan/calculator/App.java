49f1bda464abb1621e3fe56ae82245dcc2600ff2
karthiikkeyan <109920976+karthiikkeyan@users.noreply.github.com>
2024-07-18 17:55:11 +0530

    Add files via upload

---
+++ Calculator-master/Calculator-master/src/main/java/com/karthikeyan/calculator/App.java
@@ -0,0 +1,10 @@
+package com.karthikeyan.calculator;
+
+import com.karthikeyan.calculator.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
